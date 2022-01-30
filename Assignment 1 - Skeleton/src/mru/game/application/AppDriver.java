a216105650e08ddb14f5f993c001468b2a00448d
Munima <Munima@10.0.0.215>
2022-01-29 22:54:00 -0700

    test

--- Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
+++ Assignment 1 - Skeleton/src/mru/game/application/AppDriver.java
@@ -8,7 +8,7 @@ public class AppDriver {
 		// This is the starting point of the project!
 		// hint - It usually calls method from GameManager class to start the app, so only one or two lines will be written here
 		System.out.println("Test Git");
-		System.out.println("Test Git2");
+		System.out.println("Test Git5");
 	}
 
 }
